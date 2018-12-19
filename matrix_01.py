5ace16dc833bf95bf053e2435ade03bb91c3b132
Petr <petr_kovalenko_stud@mail.ru>
2018-12-19 20:16:37 +0300

    Update matrix_01.py

--- matrix_01.py
+++ matrix_01.py
@@ -1,1 +1,12 @@
 #matrix test
+class Matrix:
+    rows
+    nOfRows
+    nOfCols
+    def _init_(self, rowsArray):
+        if rowsArray is None    
+def print_matrix(matrix):
+    if matrix is None:
+        print "Error - matrix is empty!"
+        return -1
+    
