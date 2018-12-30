069c5591b2193f03ec9abce1caef6bcc431ae425
petrKovalenko <45826129+petrKovalenko@users.noreply.github.com>
2018-12-31 02:11:58 +0300

    Create chapter3.py

---
+++ chapter3.py
@@ -0,0 +1,16 @@
+#Глава 3
+#импортируем модулёк random
+import random
+
+userInput = 'n'
+
+while(userInput.lower() != 'q'):
+    dice1 = random.randint(1, 6)
+    dice2 = random.randrange(6) + 1
+    total = dice1 + dice2
+    print("Первый бросок: ", dice1)
+    print("Второй бросок: ", dice2)
+    print("Всего: ", total)
+    userInput = input("\n\nНажми q, чтобы выйти")
+
+
