dd4f9832f394bea06863d571cbd408f577e1fc06
petrKovalenko <45826129+petrKovalenko@users.noreply.github.com>
2019-01-06 16:57:50 +0300

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -1,5 +1,11 @@
 #Глава 4
 
-#Функция range
+#Функция range - сформировать массив элементов с заданным шагом
 for i in range(78):
     print(i, end=", ")
+print("\n-------")
+for j in range(1, 10):
+    print(j, end=" ")
+print("\n-------")
+for k in range(100, 0, -3):
+    print(k, end=" ")
