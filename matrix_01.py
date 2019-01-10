b3123d32f55e7828b89f0ae7ad41b0c263a9b26e
Petr <petr_kovalenko_stud@mail.ru>
2019-01-10 18:32:56 +0300

    Update matrix_01.py

--- matrix_01.py
+++ matrix_01.py
@@ -1,34 +1,37 @@
 #matrix test
 #basic matrix calss
 class Matrix(object):
-    rows
-    nOfRows
-    nOfCols
-    def _init_(self, rows_number, cols_number):
+    #rows
+    #nOfRows
+    #nOfCols
+    def __init__(self, rows_number, cols_number):
         if rows_number is None or cols_number is None:
             return -1
         self.nOfRows = rows_number
         self.nOfCols = cols_number
-        row = [None] * self.nOfRows
+        self.rows = [None] * self.nOfRows
          
-    def print_matrix():
-        if not rows :
-            print "Error - matrix rows is empty!"
+    def print_matrix(self):
+        if not self.rows :
+            print("Error - matrix rows is empty!")
             return -1
+        print("\n-----Begin Matrix---------------")
         for cur_row in self.rows:
             print(' '.join(cur_row))
+        print("-------End Matrix---------------\n")
         return 0
             
-    def insert_row(row_index, row_array):
-        if not row_index or row_index >= nOfRows:
-            print("No row in matrix with index: " + row_index)
+    def insert_row(self, row_index, row_array):
+        if row_index < 0 or row_index >= self.nOfRows:
+            print("No row in matrix with index: " + str(row_index))
             return -1
-        if not row_array or len(row_array) < nOfCols:
+        if not row_array or len(row_array) < self.nOfCols:
             print("Array of numbers is empty or too short")
             return -1
-        if len(row_array) > nOfCols:
-            print("WARN: Row array is bigger than number of matrix columns - will be added only " + nOfCols + " elements!")
-        rows[row_index] = row_array[0:nOfCols] #Отсекаем, если ввели массив большей длины
+        if len(row_array) > self.nOfCols:
+            print("WARN: Row array is bigger than number of matrix columns \
+- will be added only " + str(self.nOfCols) + " elements!")
+        self.rows[row_index] = row_array[0:self.nOfCols] #Отсекаем, если ввели массив большей длины
         return 0
 
 #Main Code
@@ -37,14 +40,20 @@ rows_number = int(input("Введите количество строк матр
 cols_number = int(input("Введите количество столбцов матрицы: "))
 if rows_number > 0 and cols_number > 0:
     matrix = Matrix(rows_number, cols_number)
-    for i in range(1, rows_number):
+    error = False
+    for i in range(0, rows_number):        
         row_string = ""
         while not row_string:
-            row_string = input("Введите " + cols_number + " элементов строки " + i + " разделённых одним пробелом")
-        result = matrix.insert_row(i - 1, row_string.split(" "))
-        if result != 0
+            count = i + 1
+            row_string = input("Введите " + str(cols_number) +" элементов строки \
+" + str(count) + " разделённых одним пробелом")
+        result = matrix.insert_row(i, row_string.split(" "))
+        #print(result)
+        if result != 0:
+            error = True
             break
-    matrix.print_matrix()
+    if not error:
+        matrix.print_matrix()
 else:
     print("Количество строк и количество столбцов матрицы длолжны быть положительынми числами!")
 
