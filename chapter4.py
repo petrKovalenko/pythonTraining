cb4ddc8da6780881c54675a9af7ceb8fe0f72b13
petrKovalenko <45826129+petrKovalenko@users.noreply.github.com>
2019-01-13 03:01:48 +0300

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -11,4 +11,21 @@ for k in range(100, 0, -3):
     print(k, end=" ")
 
 #Кортеж
-
+inventory = ()
+if not inventory:
+    print("Вы безоружны!")
+input("\nНажмите Enter, чтобы продолжить.")
+inventory = ("топор", "копьё",
+             "щит")
+print("Содержимое инвентаря:")
+print(inventory)
+for item in inventory:
+    print("Этот жалкий(ое) " + item + " вам не поможет!")
+print("В вашем распоряжении ", len(inventory), " предмет(а/ов)")
+chest = ("журнал для взрослых",)
+print("Содержимое сундука:")
+print(chest)
+inventory += chest
+print("Содержимое инвентаря:")
+print(inventory)
+input("\nНажмите Enter, чтобы выйти.")
