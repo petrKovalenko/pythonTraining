b4b1fcf921c7494c1d4c465e8bd6f4d5bd735642
petrKovalenko <45826129+petrKovalenko@users.noreply.github.com>
2019-01-14 00:13:49 +0300

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -2,7 +2,12 @@
 import random
 
 #берём слова из файла
-file = open('anagram.txt', 'r')
-
+file = open('anagram.txt', 'r', encoding='UTF8')
+text = file.read()
+print("Содержимое файла: " + text)
+#преобразование в кортеж
+words = tuple(text.split(" "))
+print("Кортеж:")
+print(words)
 
 input("\n\nНажмите Enter, чтобы выйти.")
