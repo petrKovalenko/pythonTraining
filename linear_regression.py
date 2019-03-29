fa717813b9a6b51192571d282ae1ed79cf5abe61
Petr <petr_kovalenko_stud@mail.ru>
2019-03-29 19:11:08 +0300

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,5 @@
+#линейная регрессия тест
+#входные данные
+training_values = open('regression_data/training_values.txt', 'r', encoding='UTF8')
+training_values_text = training_values.read()
+print("Содержимое файла: " + training_values_text)
