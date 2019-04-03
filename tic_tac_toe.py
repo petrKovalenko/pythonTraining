e21c867a844329c51141817d8509eef9adf1419d
Petr <petr_kovalenko_stud@mail.ru>
2019-04-03 20:25:26 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,6 +6,14 @@ EMPTY = " "
 TIE = "Ничья"
 NUM_SQUARES = 9
 
+#функция пытается преобразовать аргумент в int, если не может - возвращает null
+def intParser(num):
+    try:
+        x = int(num)
+        return x
+    except ValueError:
+        return None
+
 #возвращает инструкцию
 def instructions():
     """ Выводит на экран инструкцию для игрока """
@@ -26,12 +34,27 @@ def instructions():
 #возвращает y или n
 def ask_yes_no(question):
     answer = input(question)
-    while answer:
+    while True:
         if answer.lower() == "y"  or answer.lower() == "да" :
             return "y"
         if answer.lower() == "n"  or answer.lower() == "нет" :
             return "n"
         print("Ответ должен быть y,Y,да или n,N,нет")
         answer = input(question)
+
+#Спрашивает вопрос question, дожидаясь ответа из указанного диапазона
+#возвращает число от low до high или None в случае ошибки
+def ask_number(question, low = 0, high = 8):
+    if low > high:
+        print("Low не может быть больше high!")
+        return None
+    num = intParser(input(question))
+    while num is None or num < low or num > high:
+        print("Введите число от " + str(low) + " до " + str(high) + " включительно.")
+        num = intParser(input(question))
+    return num
+
+    
+
 
     
