3c528faf250ffb872dbf4536c3ddc46c5e22a215
Petr <p.kovalenko@i-teco.ru>
2019-07-23 19:57:12 +0300

    Create chapter7_binary_files.py

---
+++ chapter7_binary_files.py
@@ -0,0 +1,43 @@
+#Запись и чтение данных в бинарный файл
+import pickle, shelve
+#консервация - pickle
+numb = 17
+strange_string = "achtung zog!"
+cortage = ("A tebe", "есть", 18)
+my_list = [1, 2, 3, "druga"]
+my_dict = {"kill" : ["Bill", "Bob", "Quentin"], "mein" : ("teil", "hertz")}
+
+#writing
+file = open("pickle.dat", "wb")
+file_text = open("pickle_txt.dat", "wb")
+
+pickle.dump(numb, file, True)
+pickle.dump(strange_string, file, True)
+pickle.dump(cortage, file, True)
+pickle.dump(my_list, file, True)
+pickle.dump(my_dict, file, True)
+
+pickle.dump(numb, file_text, False)
+pickle.dump(strange_string, file_text, False)
+pickle.dump(cortage, file_text, False)
+pickle.dump(my_list, file_text, False)
+pickle.dump(my_dict, file_text, False)
+
+file.close()
+file_text.close()
+
+#reading
+file_r = open("pickle.dat", "rb")
+file_text_r = open("pickle_txt.dat", "rb")
+
+print("Читаем из файлов:")
+print(pickle.load(file_r))
+print(pickle.load(file_text_r))
+print(pickle.load(file_r))
+print(pickle.load(file_text_r))
+print(pickle.load(file_r))
+print(pickle.load(file_text_r))
+print(pickle.load(file_r))
+print(pickle.load(file_text_r))
+print(pickle.load(file_r))
+print(pickle.load(file_text_r))
