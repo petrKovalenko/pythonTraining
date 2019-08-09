f534a63ed2f8bd67a5a452fe93dba9094108b790
Petr <p.kovalenko@i-teco.ru>
2019-08-09 18:23:07 +0300

    Update chapter7_exception.py

--- chapter7_exception.py
+++ chapter7_exception.py
@@ -1,1 +1,9 @@
 #chapter7 - exeption example
+print("Обработка исключений - пример")
+try:
+    #test = 0 / 0
+    print(float("ahahahah"))
+except ZeroDivisionError:
+    print("Dividing by zero!")
+except (TypeError, ValueError):
+    print("Это не похоже на число")
