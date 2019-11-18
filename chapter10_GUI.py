166747071bd0ff853f004826b42fc5813bf2f00c
Petr <petr_kovalenko_stud@mail.ru>
2019-11-18 19:23:36 +0300

    py1

--- chapter10_GUI.py
+++ chapter10_GUI.py
@@ -1,1 +1,6 @@
 #chapter 10 - simple GUI
+from tkinter import *
+rootWindow = Tk()
+rootWindow.title("Окошко в кокошнике:)")
+rootWindow.geometry("100x333")
+rootWindow.mainloop()
---
+++ chapter10_exec.bat
@@ -0,0 +1,2 @@
+C:\Users\p.kovalenko\AppData\Local\Programs\Python\Python38\python.exe chapter10_GUI.py
+pause
