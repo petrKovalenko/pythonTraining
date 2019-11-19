1fda9861504f6a1e31662dc11a9f1ae4fa48abbc
Petr <petr_kovalenko_stud@mail.ru>
2019-11-19 19:16:32 +0300

    Update chapter10_GUI.py

--- chapter10_GUI.py
+++ chapter10_GUI.py
@@ -1,6 +1,22 @@
 #chapter 10 - simple GUI
 from tkinter import *
-rootWindow = Tk()
-rootWindow.title("Окошко в кокошнике:)")
-rootWindow.geometry("100x333")
-rootWindow.mainloop()
+#creation and starting window
+root_window = Tk()
+root_window.title("Окошко в кокошнике:)")
+root_window.geometry("444x333")
+#creationg frame and objects
+main_frame = Frame(root_window)
+main_frame.grid() #менеджер размещения, без него не выводится на экран
+btn1 = Button(main_frame, text="Кнопка1")
+btn1.grid()
+lb = Label(main_frame, text="Просто метка, ничего больше")
+lb.grid()
+btn2 = Button(main_frame)
+btn2.configure(text="AHAHAHAHAH!")
+btn2.grid()
+btn3 = Button(main_frame)
+btn3["text"] = "Я помню чудное...."
+btn3.grid()
+btn2.grid()
+#starting mainloop - execute window and do smthng
+root_window.mainloop()
