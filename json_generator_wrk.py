5a06cf22676e7b261305b4644b0f5d0e7c748542
Petr <petr_kovalenko_stud@mail.ru>
2020-03-31 17:32:35 +0300

    Update json_generator_wrk.py

--- json_generator_wrk.py
+++ json_generator_wrk.py
@@ -7,22 +7,64 @@ import random
 import json
 import datetime
 import math
+import string
+import copy
 
-TIME_OFFSET_MAX_SEC = 10000
+TIME_OFFSET_MAX_SEC = 100000
 RECORD_CLASSES = ("ABC", "XYZ", "LYC", "MSK")
+RECORD_MODEL = {
+    "time_key": "DATETIME",
+    "class_key":"CLASS",
+    "int_key":"INT",
+    "str_key1":"STR"
+    }
+STR_LEN_MIN = 3
+STR_LEN_MAX = 40
+
+def generateDatetime():
+    return datetime.datetime.fromtimestamp( datetime.datetime.now().timestamp() +  random.randint(-TIME_OFFSET_MAX_SEC, TIME_OFFSET_MAX_SEC))
+
+def generateInt():
+    return random.randint(-TIME_OFFSET_MAX_SEC, TIME_OFFSET_MAX_SEC)
+
+def generateClass():
+    return random.choice(RECORD_CLASSES)
+
+def generateString():
+    strLen = random.randint(STR_LEN_MIN, STR_LEN_MAX)
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(strLen))        
+
+##def generateRecord():
+##    nowTime = datetime.datetime.now().timestamp() 
+##    timeOffset = random.randint(-TIME_OFFSET_MAX_SEC, TIME_OFFSET_MAX_SEC)
+##    randTime = datetime.datetime.fromtimestamp(nowTime + timeOffset)
+##    randClass = random.choice(RECORD_CLASSES)
+##    randInt = timeOffset
+##    return {"time_key": str(randTime), "class_key": randClass, "int_key": randInt}
 
 def generateRecord():
-    nowTime = datetime.datetime.now().timestamp() 
-    timeOffset = random.randint(-TIME_OFFSET_MAX_SEC, TIME_OFFSET_MAX_SEC)
-    randTime = datetime.datetime.fromtimestamp(nowTime + timeOffset)
-    randClass = random.choice(RECORD_CLASSES)
-    randInt = timeOffset
-    return {"time_key": str(randTime), "class_key": randClass, "int_key": randInt}
+    curRecord = copy.deepcopy(RECORD_MODEL)
+    for key in curRecord.keys():
+        if curRecord[key] == "DATETIME":
+           curRecord[key] = generateDatetime();
+           continue;
+        if сurRecord[key] == "CLASS":
+            curRecord[key] = generateClass();
+            continue;
+        if сurRecord[key] == "INT":
+            curRecord[key] = generateInt();
+            continue;
+        if сurRecord[key] == "STR":
+            curRecord[key] = generateString();
+            continue;
+        #if nothing mathced, place UNKNOWN_TYPE
+        curRecord[key] == "UNKNOWN TYPE, NO MATHED VALUE"
+    return curRecord
 
 def generateFile(fileName, linesNumber):
     file = open(fileName, 'w', encoding='UTF8')
     data = []
-    for i in range(1, linesNumber):
+    for i in range(1, linesNumber+1):
         data.append(generateRecord())
     json.dump(data, file, indent=2)
     file.close()
@@ -31,12 +73,12 @@ def generateFile(fileName, linesNumber):
 if len(sys.argv) < 3:
     print("На вход подаётся минимум 2 аргумента - имя файла и количество строк!")
 else:
-    if len(sys.argv) = 3:
+    if len(sys.argv) == 3:
         generateFile(sys.argv[1], int(sys.argv[2]))
     else:
         filesAmount = int(sys.argv[3])
-        stringsPerFile = math.ceil(int(sys.argv[2] / filesAmount))
+        stringsPerFile = math.ceil(int(sys.argv[2]) / filesAmount)
         for j in range(filesAmount):
-            generateFile(sys.argv[1].replace(".", "." + (j + 1)), stringsPerFile)
+            generateFile(sys.argv[1].replace(".", str(j + 1) + "."), stringsPerFile)
             
     
