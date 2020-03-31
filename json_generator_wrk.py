849cf9baf995218eced2f9625530916645cb4970
Petr <petr_kovalenko_stud@mail.ru>
2020-03-31 17:05:13 +0300

    Update json_generator_wrk.py

--- json_generator_wrk.py
+++ json_generator_wrk.py
@@ -1,9 +1,12 @@
 #json generator
 #generates sample json for uploading in Vertica
+#На вход подаётся минимум 2 аргумента - имя файла и количество строк
+#ещё 1 аргумент - кол-во файлов
 import sys
 import random
 import json
 import datetime
+import math
 
 TIME_OFFSET_MAX_SEC = 10000
 RECORD_CLASSES = ("ABC", "XYZ", "LYC", "MSK")
@@ -15,17 +18,25 @@ def generateRecord():
     randClass = random.choice(RECORD_CLASSES)
     randInt = timeOffset
     return {"time_key": str(randTime), "class_key": randClass, "int_key": randInt}
-    
+
+def generateFile(fileName, linesNumber):
+    file = open(fileName, 'w', encoding='UTF8')
+    data = []
+    for i in range(1, linesNumber):
+        data.append(generateRecord())
+    json.dump(data, file, indent=2)
+    file.close()
+
+#-------BEGIN----------------    
 if len(sys.argv) < 3:
-    print("На вход подаётся 2 аргумента - имя файла и количество строк!")
+    print("На вход подаётся минимум 2 аргумента - имя файла и количество строк!")
 else:
-    file = open(sys.argv[1], 'w', encoding='UTF8')
-    linesNumber = int(sys.argv[2])
-    if linesNumber <= 0:
-        print("Кол-во строк дожно быть больше 0!")
+    if len(sys.argv) = 3:
+        generateFile(sys.argv[1], int(sys.argv[2]))
     else:
-        data = []
-        for i in range(1, linesNumber):
-            data.append(generateRecord())
-        json.dump(data, file, indent=2)
-        file.close()
+        filesAmount = int(sys.argv[3])
+        stringsPerFile = math.ceil(int(sys.argv[2] / filesAmount))
+        for j in range(filesAmount):
+            generateFile(sys.argv[1].replace(".", "." + (j + 1)), stringsPerFile)
+            
+    
